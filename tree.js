a1d6fadc026c30df31a728e19d6005a207024cdb
ThetPaing-7 <thatpainlin1999@gmail.com>
2026-08-30 15:08:28 +0630

    Revert "build: build_tree function to make bst" 	"build: pretty_print function to display tree structure"
    This reverts commit 7787063a3ae91607828eb21041b754455b6c1a1b.

--- tree.js
+++ tree.js
@@ -7,18 +7,20 @@ camel case
 class Tree{
     
     
-    #buildTree(array, start = 0, end = array.length -1 ){
-    
+    #buildTree(array, start = null, end = null){
+        // let uniqueArray = [...new Set(array)].sort((a,b) => a - b)
+
+        start = 0
+        end = array.length
+
         // base case
         if(start > end) return null
 
-        let mid = Math.floor((start + end ) / 2)
+        let mid = (start + end ) / 2
 
         let node = new Node(array[mid])
 
-        // create left side of tree
         node.left = this.#buildTree(array,start, mid - 1)
-        // create right side of tree
         node.right = this.#buildTree(array,mid + 1, end)
 
         return node
@@ -34,24 +36,10 @@ class Tree{
 
     showTree(){
         return this.root
-        
-    }
-
-    prettyPrint(node, prefix = '', isLeft = true){
-        // base case
-        if(node === null || node === undefined){
-            return;
-        }
-
-        this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-        this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
 }
 
 
 
 const neem = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-const tree = neem.showTree()
-neem.prettyPrint(tree)
-
+console.log(neem.showTree())
