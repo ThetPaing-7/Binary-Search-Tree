a3ce6e73ba69698dbee3f2e64c8589ecc26cf472
ThetPaing-7 <thatpainlin1999@gmail.com>
2026-08-30 17:43:17 +0630

    build: includes functions to find a values inthe tree

--- tree.js
+++ tree.js
@@ -25,12 +25,11 @@ class Tree{
     }
     
     constructor(array){
-        this.root = this.#buildTree([...new Set(array)].sort((a,b) => a - b)
-
-        )
+        this.root = this.#buildTree([...new Set(array)].sort((a,b) => a - b))
     }
 
 
+    // display values in tree format
     prettyPrint(node,prefix = '', isLeft = true){
         if (node === null || node === undefined) {
             return;
@@ -39,6 +38,32 @@ class Tree{
         this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
         console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
         this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+
+
+    // accepts values and return true if value is in tree, else false
+    includes(values,root = this.root){
+
+        if(root == null){
+            return false
+        }
+
+        let root_value = root.data
+        console.log(root_value)
+
+        // base case
+        if(root_value === values){
+            return true
+        }
+
+        if(values > root_value){
+            return this.includes(values,root.right)
+        }else if(values < root_value){
+            return this.includes(values, root.left)
+        }else{
+            return false
+        }
+    
     }
 
 
@@ -52,3 +77,5 @@ class Tree{
 const neem = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 const rootNode = neem.showTree()
 neem.prettyPrint(rootNode)
+
+console.log(neem.includes(45))
